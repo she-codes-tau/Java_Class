68a5771d90717eaf5cecf38507fca9c0068cde7e
Shani Geva <39371141+ShaniGeva12@users.noreply.github.com>
2020-09-14 20:36:06 +0300

    Lesson 5 check added

--- src/Lesson05.java
+++ src/Lesson05.java
@@ -1,2 +1,11 @@
 public class Lesson05 {
+    public boolean isPositive(double num)
+    {
+        if(num>=0)
+            return true; //no need for {} here because it's only 1 line
+
+        //no need for else here because if(true) then we go out from this function
+        //so this code will only run if the condition is false
+        return false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+
+        checkLesson5();
+    }
+
+    public static void checkLesson5()
+    {
+        double neg_num = -1.25;
+        double pos_num = 6;
+
+        Lesson05 lesson05 = new Lesson05();
+
+        System.out.println( neg_num + " is positive? ->" + lesson05.isPositive(neg_num));
+        System.out.println( pos_num + " is positive? ->" + lesson05.isPositive(pos_num));
+        pos_num = 0;
+        System.out.println( pos_num + " is positive? ->" + lesson05.isPositive(pos_num));
     }
 
 }
